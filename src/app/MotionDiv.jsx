862d7b1991a22e40c193655fe4f1923308773d43
Youssef Metry <youssef.kamal.fawzy@gmail.com>
2025-05-14 12:14:01 +0300

    Changed the MotionDiv backgound to white insted of gray

--- src/app/MotionDiv.jsx
+++ src/app/MotionDiv.jsx
@@ -5,7 +5,7 @@ const MotionDiv = dynamic(
     () => import("framer-motion").then((mod) => mod.motion.div),
     {
         ssr: false,
-        loading: () => <div className="animate-pulse bg-gray-200 rounded-lg" style={{ minHeight: "100px" }} />
+        loading: () => <div className="animate-pulse bg-white-200 rounded-lg" style={{ minHeight: "100px" }} />
     }
 );
 
